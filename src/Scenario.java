aca35788b0aaf0eaa360b7adbba24bb4b8b0fef0
AmirBrDr <amirmahdi.ghasemi002@gmail.com>
2023-02-20 10:44:29 +0100

    modify classes Gaulois,Druide and write a new Scenario

--- src/Scenario.java
+++ src/Scenario.java
@@ -2,8 +2,15 @@ import personnages.*;
 
 public class Scenario {
 	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10);
 		Gaulois asterix = new Gaulois("asterix", 8);
+		Gaulois obelix = new Gaulois("Obelix", 8);
 		Romain minus = new Romain("Minus",6);
+		panoramix.parler("e vais aller préparer une petite potion...");
+		panoramix.preparerPotion();
+		panoramix.booster(obelix);
+		obelix.parler("Par Bélénos, ce n'est pas juste !");
+		panoramix.booster(asterix);
 		asterix.parler("Bonjour a tous");
 		minus.parler("ofuisdohdch");
 		asterix.frapper(minus);
--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -24,16 +24,21 @@ public class Gaulois {
 
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup(force / 3*effetPotion);
 	}
 
 	@Override
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
 	}
-
+	public void boirePotion(int forcePotion) {
+		effetPotion=forcePotion;
+		parler("Merci Druide, je sens que ma force est " +effetPotion+ " fois décuplée.");
+	}
+	
 	public static void main(String[] args) {
-
+		Gaulois asterix = new Gaulois("asterix", 8);
+		asterix.boirePotion(6);
 	}
 
 }
